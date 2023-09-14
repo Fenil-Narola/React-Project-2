ba2bf77ed2d0f4ef52cc903611ff7e3dd0e6373c
Fenil Narola <fenilnarola0210@gmail.com>
2023-09-15 01:48:11 +0530

    Project-2

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import ContactForm from './components/ContactForm/ContactForm'
+import ContactHeader from './components/ContactHeader/ContactHeader'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <main className='main_contaniner'>
+        <ContactHeader />
+        <ContactForm />
+      </main>
+    </>
+  )
+}
+
+export default App
